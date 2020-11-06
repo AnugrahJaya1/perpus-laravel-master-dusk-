adb1d01dcaae3a55c939a5ebfdac3ab272db8d8e
muhammaddipo <muhammmaddipo@gmail.com>
2020-11-06 11:03:19 +0700

    nambah testing

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -9,18 +9,60 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 class LoginTest extends DuskTestCase
 {
     /**
-     * A Dusk test example.
+     * Test fitur login
      *
      * @return void
      */
+    // public function testLogin()
+    // {
+    //     $this->browse(function (Browser $browser) {
+    //         $browser->visit('/login')
+    //             ->type('email','admin123')
+    //             ->type('password','admin123')
+    //             ->press('Login')
+    //             ->assertPathIs('/home');
+    //     });
+    // }
+
+    // /**
+    //  * Test fitur tambah anggota
+    //  */
+    // public function testTambahAnggota(){
+
+    //     $this->browse(function (Browser $browser) {
+    //         $browser->visit('/anggota')
+    //             ->clickLink('Tambah Anggota')
+    //             ->type('nama','Muhammad Dipo')
+    //             ->type('npm','2016730091')
+    //             ->type('tempat_lahir','Bandung')
+    //             ->keys('#tgl_lahir', '4271998')
+    //             ->select('jk','L')
+    //             ->select('prodi','TI')
+    //             ->select('user_id','3')
+    //             ->press('Submit');
+    //     });
+    //     $this->assertDatabaseHas('anggota', [
+    //         'npm' => '2016730091'
+    //     ]);
+    // }
+
+    // /**
+    //  * Test fitur Log Out
+    //  */
+    // public function testLogout()
+    // {
+    //     $this->browse(function (Browser $browser) {
+    //         $browser->clickLink('Dipo')
+    //         ->clickLink('Sign Out')
+    //         ->assertPathIs('/login');
+    //     });
+    // }
     public function testLogin()
     {
+        $user = factory(User::class)->create();
         $this->browse(function (Browser $browser) {
-            $browser->visit('/login')
-                ->type('email','user123')
-                ->type('password','user123')
-                ->press('Login')
-                ->assertSee('Hello, Gilacoding');//atau bisa pakai waitForText
+            $browser->loginAs(User::find(1))
+            ->visit('/home');
         });
     }
 }
