b8296904c64f72575734bf4c85d6d18a9e6b34ce
unknown <anugrahjaya23@gmail.com>
2020-11-06 10:57:42 +0700

    menambahkan folder Auth, Anggota, dan User dan test untuk tambah user

---
+++ tests/Browser/Auth/LoginTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Browser;
+
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class LoginTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     */
+    public function testLogin()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->type('email','admin123')
+                ->type('password','admin123')
+                ->press('Login')
+                ->assertPathIs('/home');
+        });
+    }
+
+    
+}
