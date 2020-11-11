711a63b9d61ce073d8de433c5bd9e8b86fb585a2
muhammaddipo <muhammmaddipo@gmail.com>
2020-11-11 10:08:27 +0700

    nambah fitur add buku

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -27,35 +27,62 @@ class LoginTest extends DuskTestCase
     /**
      * Test fitur tambah anggota
      */
-    public function testTambahAnggota(){
+    // public function testTambahAnggota(){
 
+    //     $this->browse(function (Browser $browser) {
+    //         $browser->visit('/anggota')
+    //             ->clickLink('Tambah Anggota')
+    //             ->type('nama','Muhammad Dipo')
+    //             ->type('npm','2016730091')
+    //             ->type('tempat_lahir','Bandung')
+    //             ->keys('#tgl_lahir', '4271998')
+    //             ->select('jk','L')
+    //             ->select('prodi','TI')
+    //             ->select('user_id','3')
+    //             ->press('Submit');
+    //     });
+    //     $this->assertDatabaseHas('anggota', [
+    //         'npm' => '2016730091'
+    //     ]);
+    // }
+    /**
+     * Test fitur tambah buku (admin)
+     */
+    
+    public function testTambahBuku()
+    {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/anggota')
-                ->clickLink('Tambah Anggota')
-                ->type('nama','Muhammad Dipo')
-                ->type('npm','2016730091')
-                ->type('tempat_lahir','Bandung')
-                ->keys('#tgl_lahir', '4271998')
-                ->select('jk','L')
-                ->select('prodi','TI')
-                ->select('user_id','3')
-                ->press('Submit');
+            $browser->visit('/buku')
+            ->clickLink("Tambah Buku")
+            ->type('judul','Pemrograman Python')
+            ->type('isbn','123456789')
+            ->type('pengarang','Muhammad Dipo')
+            ->type('penerbit','PT.Muhammad Dipo')
+            ->type('tahun_terbit','2015')
+            ->type('jumlah_buku','5')
+            ->type('deskripsi','Buku untuk belajar Bahasa Pemrograman Python')
+            ->select('lokasi','rak1')
+            ->attach('cover',base_path('public/images/buku/python.png'))
+            ->press('Submit');
+
+            $this->assertDatabaseHas('buku', [
+                'isbn' => '123456789'
+            ]);
         });
-        $this->assertDatabaseHas('anggota', [
-            'npm' => '2016730091'
-        ]);
     }
 
     /**
      * Test fitur Log Out
      */
-    public function testLogout()
-    {
-        $this->browse(function (Browser $browser) {
-            $browser->clickLink('Dipo')
-            ->clickLink('Sign Out')
-            ->assertPathIs('/login');
-        });
-    }
+    // public function testLogout()
+    // {
+    //     $this->browse(function (Browser $browser) {
+    //         $browser->clickLink('Dipo')
+    //         ->clickLink('Sign Out')
+    //         ->assertPathIs('/login');
+    //     });
+    // }
+
+    
     
 }
