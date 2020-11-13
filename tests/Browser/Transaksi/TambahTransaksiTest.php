d6f3393f99d596501f0f3b2d38a0757a04fd2b16
unknown <anugrahjaya23@gmail.com>
2020-11-13 09:01:25 +0700

    coba untuk cek db, masih salah

--- tests/Browser/Transaksi/TambahTransaksiTest.php
+++ tests/Browser/Transaksi/TambahTransaksiTest.php
@@ -16,6 +16,8 @@ class TambahTransaksiTest extends DuskTestCase
     public function testExample()
     {
         $this->browse(function (Browser $browser) {
+            
+            
             $browser->visit('/login')
                 ->type('email','admin123')
                 ->type('password','admin123')
@@ -24,14 +26,18 @@ class TambahTransaksiTest extends DuskTestCase
                 ->clickLink("Tambah Transaksi")
                 ->press("Cari Buku")
                 ->waitForText("Cari Buku")
-                ->click("tr[data-buku_id='3']")
+                ->click("tr[data-buku_id='2']")
                 ->press("Cari Anggota")
                 ->waitForText("Cari Anggota")
                 ->click("tr[data-anggota_id='2']")
-                ->type('ket','Tulis keterangan disini')
+                ->type('ket',$browser->value("#kode_transaksi"))
                 ->press("Submit")
-                ->waitForText("Berhasil")
-                ->assertSee("Berhasil");
+                ->waitForText("Berhasil");
+                // ->assertSee("Berhasil");// lihat dari UI
+                $kode_transaksi = strval($browser->value("#kode_transaksi"));
+                $this->assertDatabaseHas('transaksi', [
+                    'kode_transaksi' => $kode_transaksi
+                ]);
         });
     }
 }
