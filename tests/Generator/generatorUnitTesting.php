3efd460dc563d23efb1700b52c3f7779cd63c487
unknown <anugrahjaya23@gmail.com>
2020-11-16 12:55:22 +0700

    menambahkan generator login

--- tests/Generator/generatorUnitTesting.php
+++ tests/Generator/generatorUnitTesting.php
@@ -25,7 +25,75 @@ if (is_dir($dir)) {
 
 // membuat file baru file generator
 $currPath = __DIR__;
+// $currPath ="../Browser";
 $words = preg_split('/\s+/',fgets($fileReader),-1,PREG_SPLIT_NO_EMPTY);
 $newFileName = $words[1];
 $fileWriter = fopen($currPath.'/'.$newFileName.'Test.php','w');
+
+
+// tulis ke file
+fwrite($fileWriter,"<?php\n");
+fwrite($fileWriter,"namespace Tests\Browser;\n \n");
+
+fwrite($fileWriter,"use Tests\DuskTestCase;\n");
+fwrite($fileWriter,"use Laravel\Dusk\Browser;\n");
+fwrite($fileWriter,"use Illuminate\Foundation\Testing\DatabaseMigrations;\n \n");
+
+fwrite($fileWriter,"class ".$newFileName."Test"." extends DuskTestCase { \n \n");
+
+$keys =["Scenario:", "Given", "When", "And", "Then"];
+
+
+$banyakTest=1;
+$status="";
+
+
+if($fileReader){
+    while (($line = fgets($fileReader)) !== false) {
+        $words = preg_split('/\s+/',$line,-1,PREG_SPLIT_NO_EMPTY);
+        // system("echo ".$words[0]);
+        
+        // idx untuk words
+        for($i = 0; $i < sizeof($words); $i++){
+            if($words[$i]==$keys[0]){ // Scenario:
+                fwrite($fileWriter,"public function testUnit".$banyakTest."(){\n \t");
+                fwrite($fileWriter, '$this->browse(function (Browser $browser)'. "{\n \t");
+                $banyakTest=$banyakTest+1;
+                $status=$words[$i+2];
+
+            } else if($words[$i]==$keys[1]){ // Given
+                for($j = 0; $j<sizeof($words); $j++){
+                    if($words[$j]=="halaman"){
+                        fwrite($fileWriter, '$browser->visit('."'/".$words[$j+1]."') \n \t");
+                    }
+                }
+            } else if($words[$i]==$keys[2]){ // When
+                for($j = 0; $j<sizeof($words); $j++){
+                    if($words[$j]=="username"){
+                        fwrite($fileWriter, "->type('".$words[$j]."', '".$words[$j+2]."') \n \t");
+                    }
+                }
+            } else if($words[$i]==$keys[3]){ //And
+                for($j = 0; $j<sizeof($words); $j++){
+                    if($words[$j]=="password"){
+                        fwrite($fileWriter, "->type('".$words[$j]."', '".$words[$j+2]."') \n \t");
+                    } else if($words[$j]=="tombol"){
+                        fwrite($fileWriter, "->press('Login')\n \t");
+                    }
+                }
+            } else if($words[$i]==$keys[4]){ //Then
+                for($j = 0; $j<sizeof($words); $j++){
+                    if($words[$j]=="berhasil"){
+                        fwrite($fileWriter, "->assertPathIs('/home'); \n \t}); \n} \n \n");
+                    }else if($words[$j]=="tulisan"){
+                        fwrite($fileWriter, "->assertPathIs('/login'); \n \t}); \n} \n \n");
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+fwrite($fileWriter,"}");
 ?>
