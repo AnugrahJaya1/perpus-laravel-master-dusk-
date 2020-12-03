6091863e74e5a845d78aed6683904120f098d515
muhammaddipo <muhammmaddipo@gmail.com>
2020-12-03 10:58:15 +0700

    benerin login

--- tests/Browser/Auth/LoginTest.php
+++ tests/Browser/Auth/LoginTest.php
@@ -47,87 +47,87 @@ class LoginTest extends DuskTestCase
     /**
      * Test fitur tambah anggota
      */
-    // public function testTambahAnggota(){
+    public function testTambahAnggota(){
 
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->visit('/anggota')
-    //             ->clickLink('Tambah Anggota')
-    //             ->type('nama','Muhammad Dipo')
-    //             ->type('npm','2016730091')
-    //             ->type('tempat_lahir','Bandung')
-    //             ->keys('#tgl_lahir', '4271998')
-    //             ->select('jk','L')
-    //             ->select('prodi','TI')
-    //             ->select('user_id','3')
-    //             ->press('Submit');
-    //     });
-    //     $this->assertDatabaseHas('anggota', [
-    //         'npm' => '2016730091'
-    //     ]);
-    // }
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/anggota')
+                ->clickLink('Tambah Anggota')
+                ->type('nama','Muhammad Dipo')
+                ->type('npm','2016730091')
+                ->type('tempat_lahir','Bandung')
+                ->keys('#tgl_lahir', '4271998')
+                ->select('jk','L')
+                ->select('prodi','TI')
+                ->select('user_id','3')
+                ->press('Submit');
+        });
+        $this->assertDatabaseHas('anggota', [
+            'npm' => '2016730091'
+        ]);
+    }
     /**
      * Test fitur tambah user
      */
-    // public function testTambahUser()
-    // {
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->visit('/login')
-    //             ->type('email','admin123')
-    //             ->type('password','admin123')
-    //             ->press('Login')
-    //             ->visit('/user')
-    //             ->clickLink("Tambah User")
-    //             ->type('name','Test')
-    //             ->type('username','test')
-    //             ->type('email','test@gmail.com')
-    //             ->select('level','user')
-    //             ->type('password','password')
-    //             ->type('password_confirmation','password')
-    //             ->press('Register');
+    public function testTambahUser()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/login')
+                ->type('email','admin123')
+                ->type('password','admin123')
+                ->press('Login')
+                ->visit('/user')
+                ->clickLink("Tambah User")
+                ->type('name','Test')
+                ->type('username','test')
+                ->type('email','test@gmail.com')
+                ->select('level','user')
+                ->type('password','password')
+                ->type('password_confirmation','password')
+                ->press('Register');
 
-    //             $this->assertDatabaseHas('users', [
-    //                 'username' => 'test'
-    //             ]);
-    //     });
-    // }
+                $this->assertDatabaseHas('users', [
+                    'username' => 'test'
+                ]);
+        });
+    }
 
     /**
      * Test fitur tambah buku (admin)
      */
     
-    // public function testTambahBuku()
-    // {
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->visit('/buku')
-    //         ->clickLink("Tambah Buku")
-    //         ->type('judul','Pemrograman Python')
-    //         ->type('isbn','123456789')
-    //         ->type('pengarang','Muhammad Dipo')
-    //         ->type('penerbit','PT.Muhammad Dipo')
-    //         ->type('tahun_terbit','2015')
-    //         ->type('jumlah_buku','5')
-    //         ->type('deskripsi','Buku untuk belajar Bahasa Pemrograman Python')
-    //         ->select('lokasi','rak1')
-    //         ->attach('cover',base_path('public/images/buku/python.png'))
-    //         ->press('Submit');
+    public function testTambahBuku()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/buku')
+            ->clickLink("Tambah Buku")
+            ->type('judul','Pemrograman Python')
+            ->type('isbn','123456789')
+            ->type('pengarang','Muhammad Dipo')
+            ->type('penerbit','PT.Muhammad Dipo')
+            ->type('tahun_terbit','2015')
+            ->type('jumlah_buku','5')
+            ->type('deskripsi','Buku untuk belajar Bahasa Pemrograman Python')
+            ->select('lokasi','rak1')
+            ->attach('cover',base_path('public/images/buku/python.png'))
+            ->press('Submit');
 
-    //         $this->assertDatabaseHas('buku', [
-    //             'isbn' => '123456789'
-    //         ]);
-    //     });
-    // }
+            $this->assertDatabaseHas('buku', [
+                'isbn' => '123456789'
+            ]);
+        });
+    }
 
     /**
      * Test fitur Log Out
      */
-    // public function testLogout()
-    // {
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->clickLink('Dipo')
-    //         ->clickLink('Sign Out')
-    //         ->assertPathIs('/login');
-    //     });
-    // }
+    public function testLogout()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->clickLink('Dipo')
+            ->clickLink('Sign Out')
+            ->assertPathIs('/login');
+        });
+    }
 
     
     
